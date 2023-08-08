d4b4af5bd0db6c89b6ea591558343cf05293b6d6
saimadhu2017 <madhusai2017@gmail.com>
2023-08-08 17:50:50 +0530

    Done with media quires css for search and product listing results

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { SearchProducts } from "../../components/searchProducts/SearchProducts";
 import { ProductListingResults } from "../../components/productListingResults/ProductListingResults";
+import './HomePage.css'
 
 const HomePage = () => {
     return (
         <React.Fragment>
-            <SearchProducts />
+            <div className="home_page_search_products">
+                <SearchProducts />
+            </div>
             <ProductListingResults />
         </React.Fragment>
     )
