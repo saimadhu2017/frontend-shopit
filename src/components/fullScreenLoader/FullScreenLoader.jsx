6aadc7fefa51dfebbe85e29f19c9305fae18a002
saimadhu2017 <madhusai2017@gmail.com>
2023-08-10 22:55:15 +0530

    Done with custom loader on screen

--- src/components/fullScreenLoader/FullScreenLoader.jsx
+++ src/components/fullScreenLoader/FullScreenLoader.jsx
@@ -13,6 +13,12 @@ export const FullScreenLoader = connect(mapStateToProps)(
         if (showLoader) {
             return (
                 <div className="loader-container">
+                    <div className="cs_square">
+                        <div className="cs_square1">
+                        </div>
+                        <div className="cs_square2">
+                        </div>
+                    </div>
                 </div>
             )
         }
