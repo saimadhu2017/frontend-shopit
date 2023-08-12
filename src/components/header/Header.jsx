64e5dcffaa5b94ac930d6dd56b2e436304eb5b0b
saimadhu2017 <madhusai2017@gmail.com>
2023-08-12 13:11:48 +0530

    done with dynamic header for logged in and anonymous

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,45 @@
 import './Header.css'
 import svg from '../../assets/Logo.svg'
 import Button from '../button/Button';
+import ProfileSvg from '../../assets/icons/profileSvg/ProfileSvg';
+import HomeSvg from '../../assets/icons/homeSvg/HomeSvg';
+import BagSvg from '../../assets/icons/bagSvg/BagSvg';
+import LogoutSvg from '../../assets/icons/logoutSvg/LogoutSvg';
+import { connect } from 'react-redux';
 
-const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo">
-                <Button buttonName={<img src={svg} alt="Your SVG" />} navigatePath='/' isNavigationType={true} />
-            </div>
-            <div className="nav_items">
-                <div className="item1">
-                    <Button buttonName='Sign Up' classList='signup_btn btn' navigatePath='/signup' isNavigationType={true} />
-                </div>
-                <Button buttonName='Login' classList='login_btn btn' navigatePath='/login' isNavigationType={true} />
-            </div>
-        </div>
-    )
+const mapStateToProps = (state) => {
+    return ({
+        isUserSignedin: state.updateUserSignedIn.isUserSignedin
+    })
 }
 
+const Header = connect(mapStateToProps)(
+    (props) => {
+        const { isUserSignedin } = props
+        return (
+            <div className="header">
+                <div className="logo">
+                    <Button buttonName={<img src={svg} alt="Your SVG" />} navigatePath='/' isNavigationType={true} />
+                </div>
+                {
+                    !isUserSignedin && <div className="nav_items">
+                        <div className="item1">
+                            <Button buttonName='Sign Up' classList='signup_btn btn' navigatePath='/signup' isNavigationType={true} />
+                        </div>
+                        <Button buttonName='Login' classList='login_btn btn' navigatePath='/login' isNavigationType={true} />
+                    </div>
+                }
+                {
+                    isUserSignedin && <div className='loggedin_nav_items'>
+                        <Button buttonName={<HomeSvg />} classList='loggedin_item' navigatePath='/home' isNavigationType={true} />
+                        <Button buttonName={<BagSvg />} classList='loggedin_item' navigatePath='/bag' isNavigationType={true} />
+                        <Button buttonName={<ProfileSvg />} classList='loggedin_item' navigatePath='/profile' isNavigationType={true} />
+                        <Button buttonName={<LogoutSvg />} classList='btn_remove_default_properties' isNavigationType={false} />
+                    </div>
+                }
+            </div>
+        )
+    }
+
+)
 export default Header;
